4d089f24ba73185b93aa4317e6e875fdcb5a19ff
Sergey Kozochkin <kozochkin2000@icloud.com>
2023-11-18 00:07:27 +0500

    Work2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import { createElement } from 'react';
+import React, { useState } from 'react';
+import styles from './App.module.css';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear();
+	const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	const [operand1, setOperand1] = useState('');
+	const [operand2, setOperand2] = useState('');
+	const [operator, setOperator] = useState('');
+	const [isResult, setIsResult] = useState(false);
 
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit ', createElement('code', null, 'src/App.js'), ' and save to reload.'),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('p', null, currentYear),
-		),
+	const numClick = (number) => {
+		if (isResult) {
+			setOperand1(number);
+			setIsResult(false);
+		} else {
+			if (operator) {
+				setOperand2(operand2 + number);
+			} else {
+				setOperand1(operand1 + number);
+			}
+		}
+	};
+
+	const operatorClick = (operation) => {
+		if (operation === 'C') {
+			setOperand1('');
+			setOperand2('');
+			setOperator('');
+			setIsResult(false);
+		} else if (operation === '=') {
+			if (operator && operand2) {
+				let result;
+				if (operator === '+') {
+					result = Number(operand1) + Number(operand2);
+				} else if (operator === '-') {
+					result = Number(operand1) - Number(operand2);
+				}
+				setOperand1(result.toString());
+				setOperator('');
+				setOperand2('');
+				setIsResult(true);
+			}
+		} else {
+			setOperator(operation);
+			setIsResult(false);
+		}
+	};
+
+	return (
+		<div className={styles.calc}>
+			<div className={`${styles.display} ${isResult ? styles.result : ''}`}>
+				{operand1} {operator} {operand2}
+			</div>
+			{NUMS.map((num) => (
+				<div key={num} className={styles.button} onClick={() => numClick(num)}>
+					{num}
+				</div>
+			))}
+			{['+', '-', '=', 'C'].map((operation) => (
+				<div key={operation} className={styles.button} onClick={() => operatorClick(operation)}>
+					{operation}
+				</div>
+			))}
+		</div>
 	);
 };
